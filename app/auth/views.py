c9500efbacd4fbe7636f71be8181c69396237e8f
dongil <dikwon79@naver.com>
2024-04-07 15:20:04 +0900

    change header

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,7 +8,7 @@ from ..models import User
 from sqlalchemy import exc
 from flask_mail import Message
 
-@app.after_request
+@auth.after_request
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
